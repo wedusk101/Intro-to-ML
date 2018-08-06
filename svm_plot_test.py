82e716c9323f46e64030f39c9833605ee559b77b
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-08-06 13:38:50 +0530

    Support Vector Machine WIP
    Includes demo of matplotlib

---
+++ svm_plot_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+#%matplotlib inline
+
+X = np.array([[-2, 4, -1],
+			  [4, 1, -1],
+			  [1, 6, -1],
+			  [2, 4, -1],
+			  [6, 2, -1]])
+			  
+Y = np.array([-1, -1, 1, 1, 1])
+
+for d, sample in enumerate(X):
+	if d < 2:
+		plt.scatter(sample[0], sample[1], s=120, marker='_', linewidths=2)
+	else:
+		plt.scatter(sample[0], sample[1], s=120, marker='+', linewidths=2)
+
+plt.plot([-2, 6], [6, 0.5]) # plots a line
+plt.xlabel("Gagan")
+plt.ylabel("Akash")
+plt.show()
