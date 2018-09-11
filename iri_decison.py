a81849275826168d2ce51ab437c4706581daaf6f
Gagan <goth1083@gmail.com>
2018-09-11 14:46:20 +0530

    Add files via upload
    simple decision tree for an iris dataset

---
+++ iri_decison.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+import numpy as np
+iris=load_iris()
+removed=[0,50,100]
+new_target=np.delete(iris.target,removed)
+new_data=np.delete(iris.data,removed,axis=0)
+
+clf=tree.DecisionTreeClassifier()
+
+clf=clf.fit(new_data,new_target)
+
+prediction=clf.predict(iris.data[removed])
+print ("labels predicted ",prediction)
+
